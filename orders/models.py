fbbde2c54722d0b37af323a9c9cec467b5698247
Abdukodir <carter.devper@mail.ru>
2020-01-12 20:15:44 +0500

    new models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(max_length=24, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "Statuc %s" % self.id
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    customer_emial = models.EmailField(blank=True, null=True, default=None)
+    customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
+    comments = models.TextField(blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "Order %s %s" % (self.id, self.status)
+
+
+class ProductInOrder(models.Model):
+    order = models.ForeignKey(Order, blank=True, null=True, default=None, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, blank=True, null=True, default=None, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s" % self.product.name
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductImageAdmin (admin.ModelAdmin):
+    list_display = [field.name for field in ProductImage._meta.fields]
+
+    class Meta:
+        model = ProductImage
+
+
+admin.site.register(ProductImage, ProductImageAdmin)
+
+
+
+
